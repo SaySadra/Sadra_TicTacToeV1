e412900c940b16f90b18ff5d9f68dfa0210925fe
Sadra Mousavi <saysadra@gmail.com>
2024-12-02 13:56:59 +0330

    add reset game and fix bug

--- Sadra_TicTacToeV1/Program.cs
+++ Sadra_TicTacToeV1/Program.cs
@@ -1,43 +1,79 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using BLL;
-using DAL;
+﻿using BLL;
 
 namespace Sadra_TicTacToeV1
 
 {
     internal class Program
     {
+        public static string inputString = "";
+        public static int inputNumber = 0;
+        public static int counter = 1;
+        public static string[] inputPlayer = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
         static void Main(string[] args)
         {
-            string inputString;
-            int inputNumber;
-            int counter = 1;
-            while (true)
+
+            while (inputString != "/")
             {
-                StructApplication.SetField();
+                SetField();
                 inputString = Console.ReadLine();
-                if (int.TryParse(inputString, out inputNumber) && inputNumber >=1 && inputNumber<=9)
+                if (int.TryParse(inputString, out inputNumber) && inputNumber >= 1 && inputNumber <= 9)
                 {
                     if (counter % 2 == 1)
                     {
-                        StructApplication.inputPlayer[inputNumber - 1] = "X";
-                        counter++;
+                        if (inputPlayer[inputNumber - 1] != "X" && inputPlayer[inputNumber - 1] != "O")
+                        {
+                            inputPlayer[inputNumber - 1] = "X";
+                            counter++;
+                        }
+
                     }
                     else if (counter % 2 == 0)
                     {
-                        StructApplication.inputPlayer[inputNumber - 1] = "O";
-                        counter++;
+                        if (inputPlayer[inputNumber - 1] != "X" && inputPlayer[inputNumber - 1] != "O")
+                        {
+                            inputPlayer[inputNumber - 1] = "O";
+                            counter++;
+                        }
                     }
-                    else if (counter > 9)
+                    if (counter > 9)
                     {
-                        Console.WriteLine("your game is draw!");
+                        Console.WriteLine("your game is draw!, press enter to Reset Game");
+                        Console.ReadKey();
+                        ResetGame();
+                    }
+                    if (inputString == "/")
+                    {
                         break;
                     }
                 }
+                Console.Clear();
             }
             
+            Console.ReadKey();
+        }
+        
+        public static void SetField()
+        {
+            Console.WriteLine("       |       |       ");
+            Console.WriteLine("   {0}   |   {1}   |   {2}   ", inputPlayer[0], inputPlayer[1], inputPlayer[2]);
+            Console.WriteLine("       |       |       ");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("       |       |       ");
+            Console.WriteLine("   {0}   |   {1}   |   {2}   ", inputPlayer[3], inputPlayer[4], inputPlayer[5]);
+            Console.WriteLine("       |       |       ");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("       |       |       ");
+            Console.WriteLine("   {0}   |   {1}   |   {2}   ", inputPlayer[6], inputPlayer[7], inputPlayer[8]);
+            Console.WriteLine("       |       |       ");
+            Console.WriteLine("please enter a valid number !");
+        }
+        public static void ResetGame()
+        {
+            inputPlayer = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            inputString = "";
+            inputNumber = 0;
+            counter = 1;
+            SetField();
         }
     }
 }
