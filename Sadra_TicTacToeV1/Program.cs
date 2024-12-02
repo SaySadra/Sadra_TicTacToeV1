cd3e09889908cc01ce5ae7cc15a29cfd054ad6fa
Sadra Mousavi <saysadra@gmail.com>
2024-12-02 11:18:34 +0330

    Add project files.

---
+++ Sadra_TicTacToeV1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sadra_TicTacToeV1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
